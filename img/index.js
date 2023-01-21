8b0002db5ff3d0b6880ad52e4759acf27c622076
furqanfb <123229352+furqanfb@users.noreply.github.com>
2023-01-21 13:38:06 +0530

    Add files via upload

---
+++ img/index.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+var a = 1;
+var b = 'Code with fb';
+console.log("The coder name is " + b);
+
+let myDate = new Date();
+console.log(myDate.getTime());
+// console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getHours());
